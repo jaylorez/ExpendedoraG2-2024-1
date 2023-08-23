14700cb388f48df5767b4e87e049ff2884804053
Javier <jylr.javier.lopez@gmail.com>
2023-08-23 12:13:07 -0600

    add constructor to class

--- Expendedora.cs
+++ Expendedora.cs
@@ -61,7 +61,13 @@ namespace ExpendedoraG2_2024_1
             LimpiarDisplay();
             MostrarPrecio(codigo);
         }
-        
+
+        public Expendedora(bool Mantenimiento)
+        {
+            if (Mantenimiento == true)
+                Console.WriteLine("Entrando en modo mantenimiento");
+        }
+
 
         #endregion
     }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Expendedora miExpendedora = new Expendedora();    
+        Expendedora miExpendedora = new Expendedora(); 
 
     }
 }
