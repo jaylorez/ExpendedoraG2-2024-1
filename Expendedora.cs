e18a58f3146c3ba459d5d98c78d7cda494ade600
Alumno <Alumno@L27-2543309>
2023-08-23 11:48:34 -0600

    Agregar archivos de proyecto.

---
+++ Expendedora.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+
+
+namespace ExpendedoraG2_2024_1
+{
+    internal class Expendedora
+    {
+        #region Atributos
+        private string marca;
+        private ushort cantproductos;
+        private float precio;
+        #endregion
+
+        #region Métodos
+        private void Saludar()
+        {
+            Console.WriteLine("Bienvenido, elige un producto");
+        }
+        private void LimpiarDisplay()
+        {
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+        public string MostrarProducto()
+        {
+            Console.WriteLine(" 3A: Doritos \n 3B: Churrumais ");
+            Console.WriteLine("Ingrese el código del producto");
+            string codigo = Console.ReadLine();
+            return codigo;
+        }
+
+        public void MostrarPrecio(string codigo)
+        {
+            switch(codigo)
+            {
+                case "3A":
+                    Console.WriteLine("Precio: {0}", precio);
+                    break;
+                case "3B":
+                    Console.WriteLine("Precio: {0}", precio-6);
+                    break;
+                default:
+                    Console.WriteLine("No ingresaste un producto válido");
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+        public Expendedora()
+        {
+            marca = "AMS";
+            precio = 18;
+            Saludar();
+            LimpiarDisplay();
+            Console.WriteLine(" Marca: {0}", marca);
+            LimpiarDisplay();
+            string codigo = MostrarProducto();
+            LimpiarDisplay();
+            MostrarPrecio(codigo);
+        }
+        
+
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using ExpendedoraG2_2024_1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Expendedora miExpendedora = new Expendedora();    
+
+    }
+}
